8a79fe0d392ee496d2c683025922ea3e80d77fdd
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-16 11:34:28 +0000

    Added the multiplication game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,10 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if  (gameType ==="multiply"){
+        displayMultiplicationQuestion(num1,num2);
+    }
+    else {
         alert(`Unknown game type : ${gameType}`);
         throw `unknown game type :${gameType}.Aborting!`;
     }
@@ -68,7 +71,10 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
 
-    } else {
+    } else if (operator === "x") {
+        return[operand1 * operand2, "multiply"];
+    }
+    else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
@@ -97,7 +103,9 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplicationQuestion() {
-
+function displayMultiplicationQuestion(operand1,operand2) {
+ document.getElementById("operand1").textContent =operand1;
+ document.getElementById("operand2").textContent =operand2;
+ document.getElementById ("operator").textContent ="x";
 }
 
